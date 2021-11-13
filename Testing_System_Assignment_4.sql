997a0f4471c0e51eaff84b7744c0e7b4910c834f
Trang1411 <Trangbinz1411@gmail.com>
2021-11-13 16:46:20 +0700

    update assignment 4

--- Testing_System_Assignment_4.sql
+++ Testing_System_Assignment_4.sql
@@ -58,6 +58,9 @@ VALUE
     ('TranAnh123@gmail.com', 'HaiAnh', 'TranHaiAnh', 5,2,20211108),
     ('DangAn123@gmail.com', 'ThaiAn', 'DangThaiAn', 1,3,20211107),
     ('NgoLam123@gmail.com', 'DangLam', 'NgoDanglam', 3,3,20211106),
+    ('LyAnh123@gmail.com', 'ThanhAnh', 'LyThanhAnh', 5,8,20211108),
+    ('HoangLich123@gmail.com', 'VanLich', 'HoangVanLich', 1,6,20211107),
+    ('HoHa123@gmail.com', 'NgocHa', 'HoNgocHa', 3,7,20211106),
     ('NguyenBe123@gmail.com', 'NguyenBe', 'NguyenChiBe', 2,4,20211105);
     
 CREATE TABLE `Group`(
@@ -79,7 +82,7 @@ VALUE
     
 CREATE TABLE GroupAcount (
 Group_id 			TINYINT UNSIGNED AUTO_INCREMENT,
-Account_id			TINYINT UNSIGNED UNIQUE KEY,
+Account_id			TINYINT UNSIGNED NULL,
 JoinDate 			DATE,
 FOREIGN KEY (Account_id) REFERENCES `Account`(Account_id),
 FOREIGN KEY (Group_id ) REFERENCES  `Group`(Group_id )
@@ -89,9 +92,9 @@ VALUE
 	(1,20201102		),
     (3,20201101		),
     (6,20201105		),
-    (4,20201108		),
+    (6,20201108		),
     (8,20211106		),
-    (2,20211102		),
+    (5,20211102		),
     (5,20211106		);
     
 CREATE TABLE TypeQuestion (
@@ -133,16 +136,16 @@ FOREIGN KEY (Creator_id) REFERENCES  `Account`(Account_id)
 );
 INSERT INTO Question(Content, Category_id, Type_id, Creator_id, CreateDate)
 VALUE
-	('Question1', 1, 1, 1, 20201110),
-    ('Question2', 2, 2, 1, 20201220),
-    ('Question3', 3, 3, 1, 20201110),
-    ('Question4', 4, 4, 1, 20201110),
-    ('Question5', 5, 5, 1, 20201220),
-    ('Question6', 4, 6, 1, 20201110),
-    ('Question7', 2, 5, 1, 20201110),
-    ('Question8', 1, 4, 1, 20201220),
-    ('Question9', 2, 3, 1, 20201110),
-	('Question10', 3, 2, 1, 20201110);
+	('Câu hỏi đầu tiên dễ lắm', 1, 1, 1, 20201110),
+    ('Câu hỏi 2 khó hơn tí', 2, 2, 1, 20201220),
+    ('Câu hỏi 3 khó hơn nhiều', 3, 3, 1, 20201110),
+    ('Câu hỏi 4 khó rồi', 4, 4, 1, 20201110),
+    ('Hỏi câu 5 đây', 5, 5, 1, 20201220),
+    ('Hỏi tận 6 câu', 4, 6, 1, 20201110),
+    ('Lại còn câu 7 nữa', 2, 5, 1, 20201110),
+    ('Thôi câu 8 bỏ qua', 1, 4, 1, 20201220),
+    ('Câu 9 không làm nữa', 2, 3, 1, 20201110),
+	('Câu 10 xong nộp bài :))', 3, 2, 1, 20201110);
 
 DROP TABLE IF EXISTS Answer;
 CREATE TABLE Answer(
@@ -163,7 +166,7 @@ VALUE
     ('Answer7', 8, 'UNKNOWN'	),
     ('Answer8', 8, 'TRUE'		),
     ('Answer9', 8, 'FALES'		),
-    ('Answer15', 10, 'UNKNOWN'	);
+    ('Answer9', 10, 'UNKNOWN'	);
 
 DROP TABLE IF EXISTS Exam;
 CREATE TABLE Exam (
@@ -247,11 +250,13 @@ FROM `Account`
 GROUP BY Department_id;
 
 -- Câu 5: Viết lệnh để lấy ra danh sách câu hỏi được sử dụng trong đề thi nhiều nhất 
-
-SELECT question.Question_id, COUNT(examquestion.Exam_id)
+SELECT Question_id AS 'CÂU HỎI ĐƯỢC DÙNG NHIỀU NHẤT'
 FROM examquestion
-RIGHT JOIN question ON question.Question_id = examquestion.Question_id
-GROUP BY question.Question_id;
+GROUP BY Question_id
+HAVING COUNT(Question_id) = (	SELECT  MAX(ABC)
+								FROM (	SELECT Question_id, COUNT(Question_id) AS 'ABC'
+									FROM examquestion
+									GROUP BY Question_id)AB);
 
 -- Câu 6: Thông kê mỗi category Question được sử dụng trong bao nhiêu QuestioN
 SELECT Category_id, count(Category_id) AS 'SỐ LẦN SỬ DỤNG'
@@ -264,3 +269,53 @@ FROM examquestion
 GROUP BY Question_id;
 
 -- Câu 8: Lấy ra Question có nhiều câu trả lời nhất
+ -- lấy ra số câu trả lời mỗi question 
+	SELECT count(Question_id) AS ABC
+	FROM answer
+	GROUP BY Question_id;
+-- lấy ra số lượng câu trả lời nhiều nhất 
+SELECT MAX(ABC)
+FROM (
+	SELECT Question_id, count(Question_id) AS ABC
+	FROM answer
+	GROUP BY Question_id
+	)AB;
+-- trả lời câu 8
+SELECT Question_id AS "Question có nhiều câu trả lời nhất"
+FROM answer
+GROUP BY Question_id
+HAVING  count(Question_id) = (	SELECT MAX(ABC)
+								FROM (SELECT Question_id, count(Question_id) AS ABC
+									FROM answer
+									GROUP BY Question_id)AB );
+
+-- Câu 9: Thống kê số lượng Account trong mỗi Group
+SELECT Group_id, COUNT(Account_id) AS "Số lượng Account"
+FROM groupacount
+GROUP BY Account_id;
+
+-- Câu 11: Thống kê mỗi phòng ban có bao nhiêu Dev, Test, Scrum Master, PM
+SELECT AC.Department_id,  D.Department_name, P.Position_name, count(AC.department_id)
+FROM `Account` AC
+INNER JOIN Position P ON AC.Position_id = P.Position_id
+INNER JOIN Department D ON AC.department_id = D.department_id
+GROUP BY D.department_id, P.Position_name
+ORDER BY D.department_id;
+
+-- Câu 12:  Lấy thông tin chi tiết của câu hỏi bao gồm: thông tin cơ bản của question, loại câu hỏi, ai là người tạo ra câu hỏi, câu trả lời là gì, …
+SELECT Q.Question_id, Q.Content, A.Content, TQ.TypeName, CQ.CategoryName, AC.FullName
+FROM Question Q
+LEFT JOIN Answer A ON Q.Question_id = A.Question_id
+JOIN typequestion TQ ON Q.Type_id = TQ.Type_id
+LEFT JOIN categoryquestion CQ ON Q.Category_id = CQ.Category_id
+LEFT JOIN `Account` AC ON Q.Creator_id = AC.Account_id;
+
+-- Câu 13: Lấy ra số lượng câu hỏi của mỗi loại tự luận hay trắc nghiệm
+SELECT TQ.Type_id, Q.Question_id, TQ.TypeName, COUNT(TQ.Type_id)
+FROM Question Q 
+INNER JOIN typequestion TQ ON Q.Type_id = TQ.Type_id
+WHERE TQ.TypeName ='Essay' OR TQ.TypeName = 'Multiple-Choice'
+GROUP BY TQ.Type_id
+HAVING COUNT(TQ.Type_id);
+
+-- Câu 14: 
